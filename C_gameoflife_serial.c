37fbac4d48a5c40c0a08345f8977b44747d8960e
Leonardo Silveira <sombriks@gmail.com>
2017-06-14 17:51:16 -0300

    trabalhos pro pic

--- C_gameoflife_serial.c
+++ C_gameoflife_serial.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-// #include <unistd.h>
- 
+
+#ifdef TERMINAL
+#include <unistd.h>
+#endif 
+
 #define for_x for (int x = 0; x < w; x++)
 #define for_y for (int y = 0; y < h; y++)
 #define for_xy for_x for_y
+
+const int w=25;
+const int h=25;
+unsigned int univ[w][h];
+unsigned int new[w][h];
+
+
 void show(void *u, int w, int h)
 {
-	int (*univ)[w] = u;
+	// int (*univ)[w] = u;
 	printf("\033[H");
 	for_y {
 		for_x printf(univ[y][x] ? "\033[07m  \033[m" : "  ");
@@ -18,8 +28,8 @@ void show(void *u, int w, int h)
  
 void evolve(void *u, int w, int h)
 {
-	unsigned (*univ)[w] = u;
-	unsigned new[h][w];
+	// unsigned (*univ)[w] = u;
+	//unsigned new[h][w];
  
 	for_y for_x {
 		int n = 0;
@@ -34,23 +44,19 @@ void evolve(void *u, int w, int h)
 	for_y for_x univ[y][x] = new[y][x];
 }
  
-void game(int w, int h)
+void game()
 {
-	unsigned univ[h][w];
 	for_xy univ[y][x] = rand() < RAND_MAX / 10 ? 1 : 0;
 	while (1) {
 		show(univ, w, h);
 		evolve(univ, w, h);
-//		usleep(200000);
+		#ifdef TERMINAL
+		usleep(200000);
+		#endif
 	}
 }
  
 int main(int c, char **v)
 {
-	int w = 0, h = 0;
-	if (c > 1) w = atoi(v[1]);
-	if (c > 2) h = atoi(v[2]);
-	if (w <= 0) w = 30;
-	if (h <= 0) h = 30;
-	game(w, h);
+	game();
 }
--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 gof: C_gameoflife_serial.c
-	gcc -Wall C_gameoflife_serial.c -o gof
+	gcc -DTERMINAL -Wall C_gameoflife_serial.c -o gof
 	
 gof.hex: C_gameoflife_serial.c
 	xc8 --chip=18F4550 --codeoffset=0x1000 C_gameoflife_serial.c --OUTPUT gof.hex
